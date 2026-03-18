f46301636a03fa6867c1f5fca12584696d62e8e3
htuyn <tuyenvh.ptit@gmail.com>
2026-03-18 10:21:48 +0700

    update

--- src/pages/TH04/index.tsx
+++ src/pages/TH04/index.tsx
@@ -2,32 +2,38 @@ import { PageContainer } from '@ant-design/pro-layout';
 import { Tabs } from 'antd';
 import { useModel } from 'umi'; // Hook quan trọng nhất của Umi
 import CauHinhBieuMau from './CauHinhBieuMau';
-import QuanLySoQuyetDinh from './QuanLySoQuyetDinh';
+import SoVanBangForm from './QuanLySoQuyetDinh';
+import QuyetDinhForm from './QuyetDinhForm';
 import ThongTinVanBang from './ThongTinVanBang';
 import TraCuuVanBang from './TraCuuVanBang';
+import { QuyetDinh, SoVanBang } from './utils';
+import { useState } from 'react';
+import QuanLy from './KetHop';
 
 const Bai4Main = () => {
-  // Gọi đúng tên model "localStorage" (tương ứng với tên file localStorage.ts)
-  const storageModel = useModel('localStorage');
+	// Gọi đúng tên model "localStorage" (tương ứng với tên file localStorage.ts)
+	const storageModel = useModel('TH04.localStorage');
+	const [soVanBang, setSoVanBang] = useState<SoVanBang[]>([]);
+	const [quyetDinh, setQuyetDinh] = useState<QuyetDinh[]>([]);
 
-  return (
-    <PageContainer title="HỆ THỐNG QUẢN LÝ VĂN BẰNG (TH04) - PTIT">
-      <Tabs type="card" style={{ background: '#fff', padding: 16, borderRadius: 8 }}>
-        <Tabs.TabPane tab="1. Cấu hình biểu mẫu" key="1">
-          <CauHinhBieuMau {...storageModel} />
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="2. Quyết định & Sổ" key="2">
-          <QuanLySoQuyetDinh {...storageModel} />
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="3. Thông tin văn bằng" key="3">
-          <ThongTinVanBang {...storageModel} />
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="4. Tra cứu văn bằng" key="4">
-          <TraCuuVanBang {...storageModel} />
-        </Tabs.TabPane>
-      </Tabs>
-    </PageContainer>
-  );
+	return (
+		<PageContainer title='HỆ THỐNG QUẢN LÝ VĂN BẰNG (TH04) - PTIT'>
+			<Tabs type='card' style={{ background: '#fff', padding: 16, borderRadius: 8 }}>
+				<Tabs.TabPane tab='1. Cấu hình biểu mẫu' key='1'>
+					<CauHinhBieuMau {...storageModel} />
+				</Tabs.TabPane>
+				<Tabs.TabPane tab='2. Quyết định & Sổ' key='2'>
+					<QuanLy />
+				</Tabs.TabPane>
+				<Tabs.TabPane tab='3. Thông tin văn bằng' key='3'>
+					<ThongTinVanBang {...storageModel} />
+				</Tabs.TabPane>
+				<Tabs.TabPane tab='4. Tra cứu văn bằng' key='4'>
+					<TraCuuVanBang {...storageModel} />
+				</Tabs.TabPane>
+			</Tabs>
+		</PageContainer>
+	);
 };
 
 export default Bai4Main;
